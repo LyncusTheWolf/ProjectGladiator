f86aa7baadcfdfcf7d6a0dac4d4d0107fbb99b02
LyncusTheWolf <Kenneth_anime@yahoo.com>
2018-05-03 15:42:39 -0700

    Setup the basic controller framework

---
+++ ProjectGladiator/Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gladiatorz {
+    public struct CharacterInput {
+        public float leftHorizontal;
+        public float leftVertical;
+        public Vector3 worldMotionDirection;
+        public bool jumpDown;
+    }
+
+    [System.Serializable]
+    public struct CharacterBasicStats {
+        public float moveSpeed;
+        public float moveSpeedAcceleration;
+        public float strafeSpeed;
+        public float turnSpeed;
+    }
+
+    public abstract class ControllerState {
+        public CharacterMotor parentMotor;
+
+        public virtual void OnStateEnter() { }
+        public virtual void OnStateExit() { }
+        public abstract void OnStateUpdate(CharacterInput input);
+    }
+
+    [RequireComponent(typeof(Rigidbody))]
+    public class CharacterMotor : MonoBehaviour {
+
+        public const float INPUT_DEADZONE = 0.05f;
+
+        public Animator characterAnimator;
+        public Transform focalPoint;
+        public CharacterBasicStats stats;
+
+        private CharacterInput frameInput;
+        private ControllerState currentCharacterState;
+        private new Rigidbody rigidbody;
+
+        #region Accesors
+        public Rigidbody CharacaterRigidbody {
+            get { return rigidbody; }
+        }
+
+        public CharacterInput CurrentFrameInput {
+            get { return frameInput; }
+        }
+        #endregion
+
+        public void Awake() {
+            rigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Use this for initialization
+        void Start() {
+            BuildControllerStates();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            ClearFrameInput();
+            ProcessFrameInput();
+            currentCharacterState.OnStateUpdate(frameInput);
+        }
+
+        public void BuildControllerStates() {
+            currentCharacterState = new LocomotionState();
+            currentCharacterState.parentMotor = this;
+        }
+
+        public void ClearFrameInput() {
+            frameInput.jumpDown = false;
+            frameInput.worldMotionDirection = Vector3.zero;
+        }
+
+        /// <summary>
+        /// Wrapper for the processing of frame input, this could be overwritten in an AI motor to allow it to utilize the same functionality
+        /// </summary>
+        public void ProcessFrameInput() {
+            frameInput.leftHorizontal = Input.GetAxis("Horizontal");
+            frameInput.leftVertical = Input.GetAxis("Vertical");
+            frameInput.worldMotionDirection = GetCameraRelativeInput(new Vector2(frameInput.leftHorizontal, frameInput.leftVertical), Camera.main.transform);
+        }
+
+        public Vector3 GetCameraRelativeInput(Vector2 input, Transform camera) {
+            Vector3 rootDirection = transform.forward;
+            Vector3 stickDirection = new Vector3(input.x, 0.0f, input.y);
+
+            Vector3 camDir = camera.transform.forward;
+            camDir.y = 0;
+            Quaternion relativeRotation = Quaternion.FromToRotation(Vector3.forward, Vector3.Normalize(camDir));
+
+            Vector3 moveDirection = relativeRotation * stickDirection;
+
+            return moveDirection;
+        }
+    }
+}
---
+++ ProjectGladiator/Assets/Scripts/ControllerStates/Locomotion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gladiatorz {
+    public class LocomotionState : ControllerState {
+        public float currentMoveMagnitude;
+
+        public override void OnStateEnter() {
+            currentMoveMagnitude = 0.0f;
+        }
+
+        public override void OnStateUpdate(CharacterInput input) {
+            Transform root = parentMotor.transform;
+
+            Debug.DrawRay(parentMotor.transform.position + Vector3.up, input.worldMotionDirection * 5.0f);
+
+            float inputMoveMagnitude = input.worldMotionDirection.magnitude;
+            Vector3 targetDir = input.worldMotionDirection;
+
+            if (inputMoveMagnitude < CharacterMotor.INPUT_DEADZONE) {
+                inputMoveMagnitude = 0.0f;
+                targetDir = root.forward;
+            }
+
+            currentMoveMagnitude = Mathf.Lerp(currentMoveMagnitude, input.worldMotionDirection.magnitude, Time.deltaTime * parentMotor.stats.moveSpeedAcceleration);
+            root.rotation = Quaternion.Lerp(root.rotation, Quaternion.LookRotation(targetDir), Time.deltaTime * parentMotor.stats.turnSpeed * currentMoveMagnitude);
+
+            float oldY = parentMotor.CharacaterRigidbody.velocity.y;
+            Vector3 newVelocity = root.forward * currentMoveMagnitude * parentMotor.stats.moveSpeed;
+            newVelocity.y = oldY;
+
+            parentMotor.CharacaterRigidbody.velocity = newVelocity;
+        }
+    }
+}
